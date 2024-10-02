c88231a60330726a7426fb3b7dcc1ef8d78fe94f
ennisvaleria <ennisvaleria159@gmail.com>
2024-10-02 17:48:37 -0500

    Prueba valeria

--- proyecto1/FrmLogin.Designer.cs
+++ proyecto1/FrmLogin.Designer.cs
@@ -108,11 +108,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(599, 49);
+            this.label1.Location = new System.Drawing.Point(572, 55);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 49);
+            this.label1.Size = new System.Drawing.Size(317, 49);
             this.label1.TabIndex = 8;
-            this.label1.Text = "Acceso";
+            this.label1.Text = "Acceso Valeria";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
